5352ac353d6a0704b7533bbc98b4b93754701b74
bsergean <bsergean@f59e922b-d310-0410-ab06-f75e778cbb12>
2006-04-08 02:10:27 +0000

    default values for config files

--- trunk/conf.py
+++ trunk/conf.py
@@ -1,4 +1,6 @@
 #*****************************************************************************
+# ormMainDir is the directory prefix for the future downloaded media
+# 
 # settings format: <filename prefix>,<url>
 # <filename prefix> will be used to create the dir where each podcast are put
 # ormMainDir is the prefix dir where the podcast specific dirs are created
@@ -19,23 +21,37 @@
 # for the 2006-03-21 podcast
 #*****************************************************************************
 
-ormMainDir = '/home/mp3'
-ormMainDir = '/perso/mp3/Divers/Divers - Divers'
+import os
+import sys
+
+# default dir on some platforms, see below for tuning
+if sys.platform == 'darwin':
+    ormMainDir = os.path.expanduser('~/Desktop')
+elif sys.platform == 'win32':
+    ormMainDir = 'C:/tmp'
+else: # Unix ?
+    ormMainDir = os.path.expanduser('~')
+
+########
+#
+#  Dont touch anything before above :)
+#
+########
+
+# if you want to tune it, uncomment here:
+# ormMainDir = '/home/mp3'
+# ormMainDir = '/perso/mp3/Divers/Divers - Divers'
+
+# The main parameter
 ormPodcastSettings = """
 
-#orm,http://podcast.rtl.fr/onrefaitlematch.xml
-#masque,http://radiofrance-podcast.net/podcast/rss_14007.xml
-#coffe,http://radiofrance-podcast.net/podcast/rss_10031.xml
-#fouduroi,http://radiofrance-podcast.net/podcast/rss_10048.xml
-#leplustot,http://radiofrance-podcast.net/podcast/rss_10030.xml
-#
-#orm2,http://podcast.rtl.fr/onrefaitlematch.xml
-#masque2,http://radiofrance-podcast.net/podcast/rss_14007.xml
-#coffe2,http://radiofrance-podcast.net/podcast/rss_10031.xml
-#fouduroi2,http://radiofrance-podcast.net/podcast/rss_10048.xml
-#leplustot2,http://radiofrance-podcast.net/podcast/rss_10030.xml
+orm,http://podcast.rtl.fr/onrefaitlematch.xml
+masque,http://radiofrance-podcast.net/podcast/rss_14007.xml
+coffe,http://radiofrance-podcast.net/podcast/rss_10031.xml
+fouduroi,http://radiofrance-podcast.net/podcast/rss_10048.xml
+leplustot,http://radiofrance-podcast.net/podcast/rss_10030.xml
 
-test,http://marge1/~bsergean/pod.xml
-test2,http://marge1/~bsergean/pod2.xml
-test3,http://marge1/~bsergean/pod3.xml
+#test,http://marge1/~bsergean/pod.xml
+#test2,http://marge1/~bsergean/pod2.xml
+#test3,http://marge1/~bsergean/pod3.xml
 """
